67c721124b8cbf2229181ef210c63d0ee1316ee8
Samuel Seo <ngogiaphat0802@gmail.com>
2023-07-06 03:42:24 +0700

    complete components

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,8 @@
+const {Box} = require('@mui/material');
+const {styled} = require('@mui/system');
+const FlexBetween = styled(Box)({
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "space-between",
+});
+export default FlexBetween;
