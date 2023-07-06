d8db1af7ec9c5beb1b048bb2c410fdfaa874e0fc
Samuel Seo <ngogiaphat0802@gmail.com>
2023-07-06 19:42:52 +0700

    total

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from 'react';
+import {api} from 'state/api';
+import ReactDOM from 'react-dom';
+import globalReducer from 'state';
+import {Provider} from 'react-redux';
+import {configureStore} from '@reduxjs/toolkit';
+import {setupListeners} from '@reduxjs/toolkit/dist/query';
+const store = configureStore({
+	reducer: {
+		global: globalReducer,
+		[api.reducerPath]: api.reducer,
+	},
+	middleware: (getDefault) => getDefault().contact(api.middleware),
+});
+setupListeners(store.dispatch);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<App/>
+		</Provider>
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
